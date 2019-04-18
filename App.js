c31176f757521ad9492d0c6c46c87f7f68f7d243
Anisha Jain <anishajain@outlook.com>
2019-04-18 14:31:28 -0700

    scrollview implemented

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <ScrollView
+        style={styles.scrollView}
+      >
+
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+
+        <View style={styles.box}></View>
+        <View style={styles.box}></View>
+        <View style={styles.box}></View>
+        <View style={styles.box}></View>
+
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  scrollView: {
+    flex: 1,
+    height: '100%'
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 50,
   },
+  box: {
+    height: 300,
+    margin: 10,
+    backgroundColor: '#f0f'
+  }
 });
